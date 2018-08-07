bb1120f90882f8a2936b53c473a715a46c5c3a6f
Zack Scheftel <zscheftel@berkeley.edu>
2018-08-07 13:36:57 -0700

    thing

--- news/urls.py
+++ news/urls.py
@@ -11,6 +11,5 @@ urlpatterns = [
   path('read', views.read, name = 'read'),
   path('preview', views.preview, name = 'preview'),
   path('welcome', views.welcome, name = 'welcome'),
-  path('swipe', views.swipe, name = 'swipe'),
 
 ]
